3b9525931c053ea66a2d06cb9e56822aad85953d
Elliot Davies <elliot.davies@the-times.co.uk>
2015-07-21 15:21:46 +0100

    Initial commit

---
+++ DoctopProvider.js
@@ -0,0 +1,30 @@
+/**
+ * @ngdoc service
+ * @name times.doctop.Doctop
+ * @description Provider allowing easy config and return of Doctop data in Angular.
+ */
+'use strict';
+
+angular.module('times.doctop', [])
+  .provider('Doctop', function () {
+    var doctopResponse;
+
+    var doctopOptions = {
+      callback: function(data, Doctop) {
+        doctopResponse.resolve([data, Doctop]);
+      }
+    };
+
+    // Public API for configuration
+    this.setDoctopOptions = function (opts) {
+      doctopOptions = angular.extend(doctopOptions, opts);
+    };
+
+    // Method for instantiating
+    this.$get = function ($q, $window) {
+      doctopResponse = $q.defer();
+      $window.Tabletop.init(doctopOptions);
+
+      return doctopResponse.promise;
+    };
+  });
